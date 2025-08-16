55d88b2fe36664161c7d1321313b94974940e70d
Ramsés González <gz.ramses.oc@gmail.com>
2025-08-16 20:51:19 -0300

    Add: Se modifica About y se toca temas de performance

--- src/data/data.js
+++ src/data/data.js
@@ -1,16 +1,66 @@
+import img01 from '../assets/images/01.png';
+import img02 from '../assets/images/02.png';
+import img03 from '../assets/images/03.png';
+import imgProfile from '../assets/profile.png';
+
 export const data = {
   projects: [
     {
-      name: 'Planialtimetria',
-      src: '/images/01.png',
+      name: 'Levantamiento Topográfico',
+      src: img01,
+      alt: 'Imagen de agrimensor midiendo terreno con estación total',
     },
-    { name: 'Curvas de nivel', src: '/images/02.png' },
-    { name: 'Montaje esración', src: '/images/03.png' },
+    {
+      name: 'Curvas de Nivel y Planimetría',
+      src: img02,
+      alt: 'Imagen con curvas de nivel y trabajos de cartografía',
+    },
+    {
+      name: 'Instalación de Estación de Referencia',
+      src: img03,
+      alt: 'Imagen nivelando estación GNSS para georreferenciación',
+    },
   ],
-  services: [{}],
+  services: [
+    {
+      name: 'Ciudad de Buenos Aires',
+      service: [
+        'Levantamientos topográficos',
+        'Georreferenciación de terrenos',
+        'Parcelamiento urbano',
+        'Relevamientos catastrales',
+      ],
+    },
+    {
+      name: 'Capital Federal',
+      service: [
+        'Curvas de nivel y planimetría',
+        'Asesoramiento en construcción civil',
+        'Relevamientos para licencias urbanísticas',
+        'Control de obra y topografía aplicada',
+      ],
+    },
+    {
+      name: 'Río Negro',
+      service: [
+        'Levantamientos planialtimétricos',
+        'Cartografía agrícola',
+        'Georreferenciación de proyectos rurales',
+        'Asesoramiento en obras civiles',
+        'Monitoreo de infraestructura',
+      ],
+    },
+  ],
+
   about: {
-    name: 'Diego Gonzalez Arbizu',
-    notes: 'Diego es un agrimensor profesional',
+    description:
+      'El Estudio DGA Agrimensura, radicado en Argentina, se especializa en levantamientos planialtimétricos, georreferenciación, cartografía y planificación de obras civiles y agrícolas. Contamos con tecnología de punta, como estaciones totales, GPS/GNSS y drones, para brindar información precisa y confiable a ingenieros, arquitectos y desarrolladores. Nuestro objetivo es acompañar cada proyecto desde el relevamiento inicial hasta la entrega de planos y datos geoespaciales, asegurando exactitud y eficiencia en cada trabajo.',
+    profile: {
+      name: 'Diego G. Arbizu',
+      notes:
+        'Soy agrimensor especializado en levantamientos planialtimétricos, curvas de nivel y georreferenciación. Trabajo en campo y oficina, brindando información precisa para proyectos de construcción, agricultura y desarrollo urbano en Argentina.',
+      src: imgProfile,
+    },
   },
   contact: {
     cell: '',
