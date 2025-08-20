179f74b17e8d302690349669ed7ace290d332710
Ramsés González <gz.ramses.oc@gmail.com>
2025-08-20 16:11:59 -0300

    Add: se agrega form y se modifica data

---
+++ src/data/helper.js
@@ -0,0 +1,9 @@
+import { Mail, Instagram, Facebook, MapPin, User } from '@lucide/astro';
+
+export const icons = {
+  instagram: Instagram,
+  facebook: Facebook,
+  mail: Mail,
+  location: MapPin,
+  cell: User,
+};
